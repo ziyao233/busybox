ziyao233/busybox

commit 5f84c5633663f6ee8c9cc3a4608b86d4b56b39d6

awk: fix backslash handling in sub() builtins
function                                             old     new   delta
awk_sub                                              559     544     -15

Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

editors/awk.c
@@ -2492,7 +2492,7 @@ static char *awk_printf(node *n, size_t *len)
  * store result into (dest), return number of substitutions.
  * If nm = 0, replace all matches.
  * If src or dst is NULL, use $0.
- * If subexp != 0, enable subexpression matching (\1-\9).
+ * If subexp != 0, enable subexpression matching (\0-\9).
  */
 static int awk_sub(node *rn, const char *repl, int nm, var *src, var *dest, int subexp)
 {
@@ -2520,35 +2520,32 @@ static int awk_sub(node *rn, const char *repl, int nm, var *src, var *dest, int 
 		residx += eo;
 		if (++match_no >= nm) {
 			const char *s;
-			int nbs;
+			int bslash;
 
 			/* replace */
 			residx -= (eo - so);
-			nbs = 0;
+			bslash = 0;
 			for (s = repl; *s; s++) {
-				char c = resbuf[residx++] = *s;
-				if (c == '\\') {
-					nbs++;
-					continue;
+				char c = *s;
+				if (c == '\\' && s[1]) {
+					bslash ^= 1;
+					if (bslash)
+						continue;
 				}
-				if (c == '&' || (subexp && c >= '0' && c <= '9')) {
-					int j;
-					residx -= ((nbs + 3) >> 1);
-					j = 0;
+				if ((!bslash && c == '&')
+				 || (subexp && bslash && c >= '0' && c <= '9')
+				) {
+					int n, j = 0;
 					if (c != '&') {
 						j = c - '0';
-						nbs++;
 					}
-					if (nbs % 2) {
-						resbuf[residx++] = c;
-					} else {
-						int n = pmatch[j].rm_eo - pmatch[j].rm_so;
-						resbuf = qrealloc(resbuf, residx + replen + n, &resbufsize);
-						memcpy(resbuf + residx, sp + pmatch[j].rm_so, n);
-						residx += n;
-					}
-				}
-				nbs = 0;
+					n = pmatch[j].rm_eo - pmatch[j].rm_so;
+					resbuf = qrealloc(resbuf, residx + replen + n, &resbufsize);
+					memcpy(resbuf + residx, sp + pmatch[j].rm_so, n);
+					residx += n;
+				} else
+					resbuf[residx++] = c;
+				bslash = 0;
 			}
 		}
 

testsuite/awk.tests
@@ -552,4 +552,51 @@ testing "awk = has higher precedence than == (despite what gawk manpage claims)"
 	'0\n1\n2\n1\n3\n' \
 	'' ''
 
+sq="'"
+testing 'awk gensub backslashes \' \
+	'awk '$sq'BEGIN { s="\\"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\
+\\|\\
+' \
+	'' ''
+testing 'awk gensub backslashes \\' \
+	'awk '$sq'BEGIN { s="\\\\"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\\\
+\\|\\
+' \
+	'' ''
+# gawk 5.1.1 handles trailing unpaired \ inconsistently.
+# If replace string is single \, it is used verbatim,
+# but if it is \\\ (three slashes), gawk uses "\<NUL>" (!!!), not "\\" as you would expect.
+testing 'awk gensub backslashes \\\' \
+	'awk '$sq'BEGIN { s="\\\\\\"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\\\\\
+\\\\|\\\\
+' \
+	'' ''
+testing 'awk gensub backslashes \\\\' \
+	'awk '$sq'BEGIN { s="\\\\\\\\"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\\\\\\\
+\\\\|\\\\
+' \
+	'' ''
+testing 'awk gensub backslashes \&' \
+	'awk '$sq'BEGIN { s="\\&"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\&
+&|&
+' \
+	'' ''
+testing 'awk gensub backslashes \0' \
+	'awk '$sq'BEGIN { s="\\0"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\0
+a|a
+' \
+	'' ''
+testing 'awk gensub backslashes \\0' \
+	'awk '$sq'BEGIN { s="\\\\0"; print "s=" s; print gensub("a", s, "g", "a|a") }'$sq \
+	's=\\\\0
+\\0|\\0
+' \
+	'' ''
+
 exit $FAILCOUNT

commit d595e16ce030fa0d956580996aa37a1a488e4f16

awk: fix handling of literal backslashes in replacement
According to POSIX standard, a backslash in the replacement of sub()
should be treated as a literal backslash if it is not preceded by a '&'
or another backslash. But busybox awk skips it unconditionally,
regardless of the following character. For example,

  $ echo "abc" | busybox awk 'sub(/abc/, "\\d")'
  d

where \d is expected here. This is known to break rsync's documentation
converter.

Let's check the next character before skipping the backslash, following
POSIX standard and behavior of GNU awk.

Link: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/awk.html
Link: https://github.com/RsyncProject/rsync/blob/62bb9bba022ce6a29f8c92307d5569c338b2f711/help-from-md.awk#L22
Fixes: 5f84c5633 ("awk: fix backslash handling in sub() builtins")
Signed-off-by: Yao Zi <ziyao@disroot.org>

editors/awk.c
@@ -2636,8 +2636,13 @@ static int awk_sub(node *rn, const char *repl, int nm, var *src, var *dest /*,in
 					resbuf = qrealloc(resbuf, residx + replen + n, &resbufsize);
 					memcpy(resbuf + residx, sp + pmatch[j].rm_so - start_ofs, n);
 					residx += n;
-				} else
+				} else {
+/* '\\' and '&' following a backslash keep its original meaning, any other
+ * occurrence of a '\\' should be treated as literal */
+					if (bslash && c != '\\' && c != '&')
+						resbuf[residx++] = '\\';
 					resbuf[residx++] = c;
+				}
 				bslash = 0;
 			}
 		}

testsuite/awk.tests
@@ -617,4 +617,9 @@ testing 'awk gsub erroneous word start match' \
 	'abc\n' \
 	'' ''
 
+testing 'awk sub literal backslash in replacement' \
+	'awk '$sq'sub(/abc/, "\\\d")'$sq \
+	'\d\n' \
+	'' 'abc\n'
+
 exit $FAILCOUNT